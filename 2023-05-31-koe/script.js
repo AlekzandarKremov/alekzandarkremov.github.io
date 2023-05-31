f614f6ef32a65a37fed9f309b1713a496f3266c0
Alek <112854025+AlekzandarKremov@users.noreply.github.com>
2023-05-31 11:14:02 +0300

    Update script.js

--- 2023-05-31-koe/script.js
+++ 2023-05-31-koe/script.js
@@ -2,6 +2,8 @@ var nappi = document.querySelector(".button")
 var nimi = document.querySelector(".nimi")
 
 nappi.addEventListener("click", function() {
+
 var syöte = prompt("Vaihda sivuston nimi kirjoittamalla syötekenttään")
 nimi.innerText = syöte
+
 })
